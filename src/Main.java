473e3bfdcd6327fdab15134938d0c84934060fff
banerjri <banerjri@mail.uc.edu>
2023-06-18 22:13:55 -0500

    project done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int theIntegerIs = 6;
+        if (theIntegerIs % 2 == 0)
+            System.out.println("The integer = " + theIntegerIs + " is Even");
+        else
+            System.out.println("The integer = " + theIntegerIs + " is Odd");
     }
 }
